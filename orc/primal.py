f5a3672b01469d82643b5dd3856c7e088b49d35e
Gabriele Cerizza <gabriele.cerizza@studenti.unimi.it>
2023-06-22 02:54:02 +0200

    Working on branching

--- orc/primal.py
+++ orc/primal.py
@@ -22,19 +22,28 @@ def _find_cover(A, b, method):
 
 def _inner_find_cover(A, b, method):
     cover = []
+    c = np.sum(A, axis=0)
     match method:
-        case "chvatal":
-            while (np.any(b > 0)):
+        case "greedy":
+            while np.any(b > 0):
+            
+                # Pick the row that is easiest to cover,
+                # since it has the largest ratio between the
+                # sum of the coefficients on the left hand 
+                # side and b.
                 M = [sum(A[i]) / (b_i or EPS)
                      for i, b_i in enumerate(b)]
                 i = np.argmax(M)
+                
+                # Pick the column that covers the most.
                 j = np.argmax(A[i])
+                
                 cover.append(j)
                 b = b - A[:,j]
                 A[:,j] -= A[:,j]
 
         case "dobson":
-            while (np.any(b > 0)):
+            while np.any(b > 0):
                 A = np.minimum(A.T, b).T
 
                 # To avoid division by zero, we copy
@@ -42,17 +51,24 @@ def _inner_find_cover(A, b, method):
                 A2 = np.copy(A).astype(np.float32)
                 A2[A2 == 0] = EPS
 
-                k = np.argmin(1 / np.sum(A2, axis=0))
+                # c is already the sum of A over axis 0,
+                # so, unless some entries of A are
+                # decreased in the first step of the while
+                # loop, all non-zero columns will have the
+                # same ratio. 
+                k = np.argmin(c / np.sum(A2, axis=0))
+                
                 cover.append(k)
                 b = b - A[:,k]
                 A[:,k] -= A[:,k]
 
         case "hall_hochbaum":
-            while (np.any(b > 0)):
+            while np.any(b > 0):
                 rsum = np.sum(A, axis=-1)
                 space = rsum - b
                 
-                f = [sum([(b[i] * A[i][j]) / (space[i]) 
+                f = [1 / (c[j] or EPS) * 
+                     sum([(b[i] * A[i][j]) / (space[i]) 
                         for i in np.where(b > 0)[0]]) 
                         for j in range(A.shape[-1])]
 
@@ -64,8 +80,8 @@ def _inner_find_cover(A, b, method):
     return cover
 
 
-def chvatal(A, b):
-    return _find_cover(A, b, "chvatal")
+def greedy(A, b):
+    return _find_cover(A, b, "greedy")
 
 
 def dobson(A, b):
@@ -75,5 +91,4 @@ def dobson(A, b):
 def hall_hochbaum(A, b):
     return _find_cover(A, b, "hall_hochbaum")
 
-#TODO: add naive, all columns?
 #TODO: hall_hochbaum gets better than dobson the bigger the problem
